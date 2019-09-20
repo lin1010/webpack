25be833942cc9624958206983639d18f78b21935
Xiaoling XL4 Lin <linxl4@Lenovo.com>
2019-09-20 16:58:22 +0800

    HMR

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.svg';
-import Data from './data.xml';
+import printMe from './print.js';
 function component() {
   var element = document.createElement('div');
-
-  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-  // Lodash, now imported by this script
+  var btn = document.createElement('button');
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-  //将图片添加到我们现有的div
-  var myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
-  console.log(Data);
+  btn.innerHTML = 'click me and check the console';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
   return element;
 }
 
-document.body.appendChild(component());
+// document.body.appendChild(component());
+let element = component(); // 当 print.js 改变导致页面重新渲染时，重新获取渲染的元素
+document.body.appendChild(element);
+
+if(module.hot) {
+  module.hot.accept('./print.js', function() {
+    console.log('Accepting the updated printMe module!');
+    // printMe()
+    document.body.removeChild(element);
+    element = component(); // 重新渲染页面后，component 更新 click 事件处理
+    document.body.appendChild(element);
+  })
+}
